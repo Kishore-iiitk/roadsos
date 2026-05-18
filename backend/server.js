c4766de9fb289cfc961e8dde220a51522a85cac5
Kishore2418 <kishoreofficial2418@gmail.com>
2026-05-18 16:47:05 +0530

    Implemented realtime ambulance tracking using Socket.IO

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 
 const express = require("express");
+const http = require("http");
+
+const { Server } = require("socket.io");
 
 const connectDB = require("./config/db");
 
@@ -8,6 +11,14 @@ const sosRoutes = require("./routes/sosRoutes");
 const hospitalRoutes = require("./routes/hospitalRoutes");
 
 const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 const PORT = process.env.PORT || 5000;
 
@@ -23,6 +34,20 @@ app.get("/", (req, res) => {
   res.send("RoadSoS Backend Running");
 });
 
-app.listen(PORT, () => {
+io.on("connection", (socket) => {
+  console.log("New client connected:", socket.id);
+
+  socket.on("ambulanceLocationUpdate", (data) => {
+    console.log("Live ambulance location:", data);
+
+    io.emit("ambulanceLocationUpdated", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
---
+++ backend/testSocket.js
@@ -0,0 +1,24 @@
+const { io } = require("socket.io-client");
+
+const socket = io("http://localhost:5000");
+
+socket.on("connect", () => {
+  console.log("Connected to server");
+
+  setInterval(() => {
+    const ambulanceData = {
+      emergencyId: "12345",
+      latitude: 12.9716 + Math.random() * 0.01,
+      longitude: 80.2212 + Math.random() * 0.01,
+      timestamp: new Date(),
+    };
+
+    socket.emit("ambulanceLocationUpdate", ambulanceData);
+
+    console.log("Sent ambulance location:", ambulanceData);
+  }, 3000);
+});
+
+socket.on("ambulanceLocationUpdated", (data) => {
+  console.log("Received realtime update:", data);
+});
